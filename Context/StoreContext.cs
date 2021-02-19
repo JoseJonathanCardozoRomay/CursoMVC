c49ca80bb6ecff7dbf4f96dac555a198eb9f6191
JoseJonathanCardozoRomay <cardozojose316@gmail.com>
2021-02-18 21:15:30 -0400

    Primer proyecto

---
+++ Context/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC1.Context
+{
+    public class StoreContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
